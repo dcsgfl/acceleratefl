2b7f39c152fa9e85ea98092da9e77aa17806520b
Nick <nikhs247@gmail.com>
2021-08-19 18:42:44 -0500

    loss scheduler with py, pxy distribution

--- server/scheduler/lossScheduler.py
+++ server/scheduler/lossScheduler.py
@@ -1,32 +1,70 @@
+import os
+import sys
+import numpy as np
+import copy
+
+pwd = os.path.join(os.path.abspath(os.path.dirname(__file__)), '..', '..', 'common','summary')
+sys.path.append(pwd)
+
+from cluster import cluster_hist
+from hist import HistSummary
+
 class LOSSched:
     def __init__(self):
         pass
 
-    def select_worker_instances(_epoch, client_list, num_of_selected, dev_profile, evalloss):
-        if(_epoch==1):
-            return(client_list)
+    def select_worker_instances(self, available_devices, client_threshold):
 
-        sorted_evalloss={k: v for k, v in sorted(evalloss.items(), key=lambda item: item[1])}
-        devids=[k for k in sorted_evalloss]
-        devloss=[sorted_evalloss[k] for k in sorted_evalloss]
-        devids=[devids[i:i+2] for i in range(0, len(devids), 2)]
-        devloss=[devloss[i:i+2] for i in range(0, len(devloss), 2)]
+        summaries = []
+        keyspace = set([])
 
-        # print(devids)
-        # print(devloss)
+        # We need a deterministic ordering of keys
+        dev_keys = copy.deepcopy(list(available_devices.keys()))
 
-        selected_devid=[]
-        for group in devids:
-            if(dev_profile[group[0]]>dev_profile[group[1]]):
-                selected_devid.append(group[1])
-            else:
-                selected_devid.append(group[0])
+        for devId in dev_keys:
+            device = available_devices[devId]
+            histSummary = HistSummary()
+            histSummary.fromJson(device['summary'])
+            summaries.append(histSummary)
+            keyspace = keyspace.union(histSummary.getKeys())
+            
+        dev_clusters = cluster_hist(summaries, list(keyspace))
 
-        res=[]
-        for dev in client_list:
-            if(dev.id in selected_devid):
-                res.append(dev)
-        return (res)
+        for idx, devId in enumerate(dev_keys):
+            available_devices[devId]['cluster'] = dev_clusters[idx]
+
+        # This is a little slow... (n^2) but just moving on for now
+        selected_devices = {}
+        for clusterId in set(dev_clusters): # For each identified cluster
+
+            bestDev = {}
+            for devId in dev_keys:          # For each device in that cluster
+
+                curDev = available_devices[devId].copy()
+    
+                if curDev['cluster'] == clusterId:
+
+                    if clusterId == -1:
+                        selected_devices[devId] = curDev.copy()
+                    else:
+                        if len(bestDev.keys()) == 0:
+                            # This is the first device in cluster
+                            bestDev = curDev.copy()
+                        elif curDev["loss"] > bestDev["loss"]:
+                            # Select device with highest loss
+                            bestDev = curDev.copy()
+
+                # TODO: these keys are also available to us...
+                #available_devices[request.id]['cpu_usage']
+                #available_devices[request.id]['ncpus']
+                #available_devices[request.id]['load']
+                #available_devices[request.id]['virtual_mem']
+                #available_devices[request.id]['battery']
+
+            if clusterId != -1:
+                selected_devices[bestDev["id"]] = bestDev.copy()
+
+        return selected_devices
 
     class Factory:
         def get(self):
