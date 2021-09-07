982aa39761f902fe61a70350684b635563f70dba
Nick <nikhs247@gmail.com>
2021-09-07 15:28:15 +0000

    py beta

--- client/run_client.py
+++ client/run_client.py
@@ -218,7 +218,7 @@ def parse_arguments(args = sys.argv[1:]):
         '--summary',
         '-s',
         type=str,
-        default='rnd',
+        default='py',
         help='data summary to send: --summary py'
     )
 
