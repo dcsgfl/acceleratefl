4677cbf80fb334494a7af0bcf8b8fb8e37be424a
Nick <nikhs247@gmail.com>
2021-09-14 14:25:27 +0000

    pxy log

--- server/run_server_v2.py
+++ server/run_server_v2.py
@@ -418,7 +418,7 @@ def parse_arguments(args = sys.argv[1:]):
     parser.add_argument(
         '--scheduler',
         type = str,
-        default = 'PYSched',
+        default = 'PXYSched',
         help = 'Scheduler type',
     )
 
