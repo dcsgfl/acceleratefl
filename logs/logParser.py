73ebaa41b0b64e494afb92116543b4eae78de83d
Nick <nikhs247@gmail.com>
2021-09-21 10:36:44 -0500

    cifar10 combined graph

--- logs/logParser.py
+++ logs/logParser.py
@@ -5,11 +5,13 @@ rndfh = open("cifar10_rnd.log")
 pyfh = open("cifar10_py.log")
 pxyfh = open("cifar10_pxy.log")
 oortfh = open("cifar10_oort.log")
+tiflfh = open("cifar10_tifl.log")
 
 rndcontent = rndfh.readlines()
 pycontent = pyfh.readlines()
 pxycontent = pxyfh.readlines()
 oortcontent = oortfh.readlines()
+tiflcontent = tiflfh.readlines()
 
 lineIdx = 0
 nLines = 170
@@ -29,6 +31,10 @@ pxytimes = []
 oortepoch = []
 oortaccuracy = []
 oorttimes = []
+
+tiflepoch = []
+tiflaccuracy = []
+tifltimes = []
 while (lineIdx < nLines):
     currLine = rndcontent[lineIdx]
     splitLine = currLine.split()
@@ -54,16 +60,23 @@ while (lineIdx < nLines):
     oortaccuracy.append(float(splitLine[3]))
     oorttimes.append(float(splitLine[17]))
 
+    currLine = tiflcontent[lineIdx]
+    splitLine = currLine.split()
+    tiflepoch.append(float(splitLine[1]))
+    tiflaccuracy.append(float(splitLine[3]))
+    tifltimes.append(float(splitLine[17]))
+
     lineIdx += 1
 
 rndctime = np.cumsum(rndtimes)
 pyctime = np.cumsum(pytimes)
 pxyctime = np.cumsum(pxytimes)
 oortctime = np.cumsum(oorttimes) 
-maxtime = max(max(rndctime), max(pyctime), max(pxyctime), max(oortctime))
+tiflctime = np.cumsum(tifltimes) 
+maxtime = max(max(rndctime), max(pyctime), max(pxyctime), max(oortctime), max(tiflctime))
 # maxtime = max(max(rndctime), max(pyctime), max(pxyctime))
 
-print(max(rndctime), max(pyctime), max(pxyctime), max(oortctime))
+print(max(rndctime), max(pyctime), max(pxyctime), max(oortctime), max(tiflctime))
 # print(max(rndctime), max(pyctime), max(pxyctime))
 
 x_ticks = np.arange(0, 180, 10)
@@ -72,6 +85,7 @@ plt.plot(rndepoch, rndaccuracy, label='random')
 plt.plot(rndepoch, pyaccuracy, label = 'py')
 plt.plot(rndepoch, pxyaccuracy, label ='pxy')
 plt.plot(rndepoch, oortaccuracy, label = 'oort')
+plt.plot(rndepoch, tiflaccuracy, label = 'tifl')
 plt.xticks(x_ticks)
 plt.xlabel('Epochs')
 plt.yticks(y_ticks)
@@ -86,6 +100,7 @@ plt.plot(rndctime, rndaccuracy, label = 'random')
 plt.plot(pyctime, pyaccuracy, label = 'py')
 plt.plot(pxyctime, pxyaccuracy, label = 'pxy')
 plt.plot(oortctime, oortaccuracy, label = 'oort')
+plt.plot(tiflctime, tiflaccuracy, label = 'tifl')
 plt.xticks(x_ticks)
 plt.xlabel('Time taken (sec)')
 plt.yticks(y_ticks)
