234a894b34da5a6f4e24d501d6a31b001970a889
Joel Wolfrath <jwolfrath@gmail.com>
2021-09-25 02:50:52 +0000

    Use hellinger distance for clustering

--- common/summary/cluster.py
+++ common/summary/cluster.py
@@ -12,6 +12,23 @@ from numpy.linalg import norm
 from sklearn.cluster import DBSCAN
 from sklearn.cluster import OPTICS
 
+def hdist(v1, v2):
+
+    sqrt2 = np.sqrt(2)
+
+    sqrtdevP = np.sqrt(v1)
+    sqrtdevQ = np.sqrt(v2)
+    sumOfSqrOfDiffSqrtdevice = np.sum((sqrtdevP - sqrtdevQ) ** 2)
+    return np.sqrt(sumOfSqrOfDiffSqrtdevice) / sqrt2
+
+def mnorm(m1, m2):
+
+    avgDist = 0.0
+    for x in range(m1.shape[0]):
+        d = hdist(m1[x,:], m2[x,:])
+        avgDist += d
+    return avgDist / float(m1.shape[0])
+
 """
 Basic clustering routine for clustering histograms.
 Searches for clusters of at least 2 points. Returns an array of assignments
@@ -23,12 +40,29 @@ def cluster_hist(histogramList, keySpace):
     for idx, hist in enumerate(histogramList):
         X[idx,:] = hist.toFrequencyArray(keySpace)
 
-    return OPTICS(min_samples=2).fit_predict(X)
-#    model = DBSCAN(eps=0.30, min_samples=2)
-#    yhat = model.fit_predict(X)
+    dim = len(histogramList)
+    distMat = np.zeros((dim, dim))
 
-#    return yhat
+    for i in range(dim):
+        for j in range(i, dim):
+            #h1 = histogramList[i]
+            #h2 = histogramList[j]
+            #dist = h1.computeHellingerDist(h2, keySpace)
+            dist = hdist(X[i,:], X[j,:])
+            distMat[i,j] = dist
+            distMat[j,i] = dist
 
+    #return DBSCAN(min_samples=2,eps=0.33,
+    #              metric="precomputed").fit_predict(distMat)
+    return OPTICS(min_samples=2,
+                  metric="precomputed").fit_predict(distMat)
+
+    #return OPTICS(min_samples=2).fit_predict(X)
+    #return OPTICS(min_samples=2).fit_predict(X)
+    #model = DBSCAN(eps=0.30, min_samples=2)
+    #yhat = model.fit_predict(X)
+
+    #return yhat
 
 """
 Basic clustering routine for clustering a list of histograms.
@@ -47,10 +81,18 @@ def cluster_mat(matList, xKeySpace, yKeySpace):
 
     for i in range(dim):
         for j in range(i, dim):
-            dist = norm(mats[i] - mats[j])
+            #dist = norm(mats[i] - mats[j], 2)
+            dist = mnorm(mats[i], mats[j])
             distMat[i,j] = dist
             distMat[j,i] = dist
 
     return OPTICS(min_samples=2,
                   metric="precomputed").fit_predict(distMat)
+    #return DBSCAN(min_samples=2,eps=0.33,
+    #              metric="precomputed").fit_predict(distMat)
+    #model = DBSCAN(eps=1.13, min_samples=2,
+    #               metric='precomputed')
+    #yhat = model.fit_predict(distMat)
+    #
+    #return yhat
 
