92036f5ebbe09ca172b0b9d04930f5301f7bbb2a
Nick <nikhs247@gmail.com>
2021-09-28 22:14:23 +0000

    comm issue resolved

--- client/run.sh
+++ client/run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-END = $1
-for ((i = 0; i < $END; i++)); do
+END=$1
+i=0
+while [ "$i" -le "$END" ]; do
     python3 run_client.py --dataset "$2" --summary "$3" --centralip "$4" &
+    i=$((i + 1))
 done
 wait
