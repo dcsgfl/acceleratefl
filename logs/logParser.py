24448c384fe8a17302beecfe06c9367114b108c6
Nick <nikhs247@gmail.com>
2021-10-01 12:10:49 -0500

    log parse

--- logs/logParser.py
+++ logs/logParser.py
@@ -1,27 +1,22 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from numpy.lib.polynomial import poly
+from scipy.signal import savgol_filter
 
-# rndfh = open("rnd_dist.log")
-# pyfh = open("py_dist.log")
-# pxyfh = open("pxy_dist.log")
-# oortfh = open("oort_dist.log")
-
-# rndfh = open("rnd_beta.log")
-# pyfh = open("py_beta.log")
-# pxyfh = open("pxy_beta.log")
-# oortfh = open("oort_beta_a15.log")
-
-rndfh = open("rnd_13_1.log")
-pyfh = open("py_13_1.log")
-pxyfh = open("pxy_13_1.log")
+rndfh = open("femnist_perf_rnd.log")
+pyfh = open("femnist_perf_py.log")
+pxyfh = open("femnist_perf_pxy.log")
+oortfh = open("femnist_perf_oort.log")
+tiflfh = open("femnist_perf_tifl.log")
 
 rndcontent = rndfh.readlines()
 pycontent = pyfh.readlines()
 pxycontent = pxyfh.readlines()
-# oortcontent = oortfh.readlines()
+oortcontent = oortfh.readlines()
+tiflcontent = tiflfh.readlines()
 
 lineIdx = 0
-nLines = 170
+nLines = 200
 
 rndepoch = []
 rndaccuracy = []
@@ -35,9 +30,13 @@ pxyepoch = []
 pxyaccuracy = []
 pxytimes = []
 
-# oortepoch = []
-# oortaccuracy = []
-# oorttimes = []
+oortepoch = []
+oortaccuracy = []
+oorttimes = []
+
+tiflepoch = []
+tiflaccuracy = []
+tifltimes = []
 while (lineIdx < nLines):
     currLine = rndcontent[lineIdx]
     splitLine = currLine.split()
@@ -57,30 +56,43 @@ while (lineIdx < nLines):
     pxyaccuracy.append(float(splitLine[3]))
     pxytimes.append(float(splitLine[17]))
 
-    # currLine = oortcontent[lineIdx]
-    # splitLine = currLine.split()
-    # oortepoch.append(float(splitLine[1]))
-    # oortaccuracy.append(float(splitLine[3]))
-    # oorttimes.append(float(splitLine[17]))
+    currLine = oortcontent[lineIdx]
+    splitLine = currLine.split()
+    oortepoch.append(float(splitLine[1]))
+    oortaccuracy.append(float(splitLine[3]))
+    oorttimes.append(float(splitLine[17]))
+
+    currLine = tiflcontent[lineIdx]
+    splitLine = currLine.split()
+    tiflepoch.append(float(splitLine[1]))
+    tiflaccuracy.append(float(splitLine[3]))
+    tifltimes.append(float(splitLine[17]))
 
     lineIdx += 1
 
+window = 19
+polyorder = 3
+rndaccuracysmooth = savgol_filter(rndaccuracy, window, polyorder)
+pyaccuracysmooth = savgol_filter(pyaccuracy, window, polyorder)
+pxyaccuracysmooth = savgol_filter(pxyaccuracy, window, polyorder)
+oortaccuracysmooth = savgol_filter(oortaccuracy, window, polyorder)
+tiflaccuracysmooth = savgol_filter(tiflaccuracy, window, polyorder)
 rndctime = np.cumsum(rndtimes)
 pyctime = np.cumsum(pytimes)
 pxyctime = np.cumsum(pxytimes)
-# oortctime = np.cumsum(oorttimes) 
-# maxtime = max(max(rndctime), max(pyctime), max(pxyctime), max(oortctime))
-maxtime = max(max(rndctime), max(pyctime), max(pxyctime))
+oortctime = np.cumsum(oorttimes)
+tiflctime = np.cumsum(tifltimes) 
+maxtime = max(max(rndctime), max(pyctime), max(pxyctime), max(oortctime), max(tiflctime))
 
-# print(max(rndctime), max(pyctime), max(pxyctime), max(oortctime))
-print(max(rndctime), max(pyctime), max(pxyctime))
+print(max(rndctime), max(pyctime), max(pxyctime), max(oortctime), max(tiflctime))
 
-x_ticks = np.arange(0, 180, 10)
+x_ticks = np.arange(0, 200, 10)
 y_ticks = np.arange(0, 1, 0.1)
-plt.plot(rndepoch, rndaccuracy, label='random')
-plt.plot(rndepoch, pyaccuracy, label = 'py')
-plt.plot(rndepoch, pxyaccuracy, label ='pxy')
-# plt.plot(rndepoch, oortaccuracy, label = 'oort')
+plt.plot(rndepoch, rndaccuracysmooth, label='random')
+plt.plot(rndepoch, pyaccuracysmooth, label = 'py')
+plt.plot(rndepoch, pxyaccuracysmooth, label ='pxy')
+plt.plot(rndepoch, oortaccuracysmooth, label = 'oort')
+plt.plot(rndepoch, tiflaccuracysmooth, label = 'tifl')
 plt.xticks(x_ticks)
 plt.xlabel('Epochs')
 plt.yticks(y_ticks)
@@ -89,12 +101,13 @@ plt.title('Accuracy vs Epoch')
 plt.legend()
 plt.show()
 
-x_ticks = np.arange(0, maxtime, 500)
+x_ticks = np.arange(0, maxtime, 1500)
 y_ticks = np.arange(0, 1, 0.1)
-plt.plot(rndctime, rndaccuracy, label = 'random')
-plt.plot(pyctime, pyaccuracy, label = 'py')
-plt.plot(pxyctime, pxyaccuracy, label = 'pxy')
-# plt.plot(oortctime, oortaccuracy, label = 'oort')
+plt.plot(rndctime, rndaccuracysmooth, label = 'random')
+plt.plot(pyctime, pyaccuracysmooth, label = 'py')
+plt.plot(pxyctime, pxyaccuracysmooth, label = 'pxy')
+plt.plot(oortctime, oortaccuracysmooth, label = 'oort')
+plt.plot(tiflctime, tiflaccuracysmooth, label = 'tifl')
 plt.xticks(x_ticks)
 plt.xlabel('Time taken (sec)')
 plt.yticks(y_ticks)
