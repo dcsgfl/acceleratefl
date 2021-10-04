6bc5da7c2d7d5688c509731317a334de39ecef2b
Nick <nikhs247@gmail.com>
2021-10-04 15:53:07 -0500

    run.sh

--- server/run.sh
+++ server/run.sh
@@ -1,5 +1,11 @@
-python3 run_server.py --dataset "$1" --scheduler "$2"
+DATASET="CIFAR10"
+SCHEDULER="RNDSched"
+DROP=10
+EPOCHS=200
+CLTHRES=10
+
+python3 run_server.py --dataset "${1:$DATASET}" --scheduler "${2:$SCHEDULER}" --drop "${3:$DROP}" --epochs "${4:$EPOCHS}" --threshold "${5:$CLTHRES}"
 
 # Sample command
 # sh run.sh dataset scheduler
-# sh run.sh FEMNIST RNDSched
+# sh run.sh FEMNIST RNDSched 10 200 10
