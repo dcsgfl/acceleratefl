be1d25ddcf7daeeaba0c932946d8bd87e356d436
Nick <nikhs247@gmail.com>
2021-10-04 21:35:20 -0500

    remove logs and change run.sh

--- server/run.sh
+++ server/run.sh
@@ -4,7 +4,7 @@ DROP=10
 EPOCHS=200
 CLTHRES=10
 
-python3 run_server.py --dataset "${1:$DATASET}" --scheduler "${2:$SCHEDULER}" --drop "${3:$DROP}" --epochs "${4:$EPOCHS}" --threshold "${5:$CLTHRES}"
+python3 run_server.py --dataset "${1:-$DATASET}" --scheduler "${2:-$SCHEDULER}" --drop "${3:-$DROP}" --epochs "${4:-$EPOCHS}" --threshold "${5:-$CLTHRES}"
 
 # Sample command
 # sh run.sh dataset scheduler
