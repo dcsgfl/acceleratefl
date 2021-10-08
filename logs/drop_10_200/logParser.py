6cef22e2b31f38145d56f87e8cff1f99ed2c0f30
csgsa <cs-gsa@umn.edu>
2021-10-08 18:54:39 -0500

    logs

--- logs/drop_10_200/logParser.py
+++ logs/drop_10_200/logParser.py
@@ -3,11 +3,13 @@ import matplotlib.pyplot as plt
 from numpy.lib.polynomial import poly
 from scipy.signal import savgol_filter
 
+rndfh = open("drop_rnd_10_200.log")
 pyfh = open("drop_py_10_200.log")
 pxyfh = open("drop_pxy_10_200.log")
 tiflfh = open("drop_tifl_10_200.log")
 oortfh = open("drop_oort_10_200.log")
 
+rndcontent = rndfh.readlines()
 pycontent = pyfh.readlines()
 pxycontent = pxyfh.readlines()
 tiflcontent = tiflfh.readlines()
@@ -15,6 +17,10 @@ oortcontent = oortfh.readlines()
 
 lineIdx = 0
 nLines = 200
+
+rndepoch = []
+rndaccuracy = []
+rndtimes = []
 
 pyepoch = []
 pyaccuracy = []
@@ -32,6 +38,12 @@ oortepoch = []
 oortaccuracy = []
 oorttimes = []
 while (lineIdx < nLines):
+    currLine = rndcontent[lineIdx]
+    splitLine = currLine.split()
+    rndepoch.append(float(splitLine[1]))
+    rndaccuracy.append(float(splitLine[3]))
+    rndtimes.append(float(splitLine[17]))
+
     currLine = pycontent[lineIdx]
     splitLine = currLine.split()
     pyepoch.append(float(splitLine[1]))
@@ -60,20 +72,22 @@ while (lineIdx < nLines):
 
 window = 101
 polyorder = 3
+rndaccuracysmooth = savgol_filter(rndaccuracy, window, polyorder)
 pyaccuracysmooth = savgol_filter(pyaccuracy, window, polyorder)
 pxyaccuracysmooth = savgol_filter(pxyaccuracy, window, polyorder)
 tiflaccuracysmooth = savgol_filter(tiflaccuracy, window, polyorder)
 oortaccuracysmooth = savgol_filter(oortaccuracy, window, polyorder)
 
+rndctime = np.cumsum(rndtimes)
 pyctime = np.cumsum(pytimes)
 pxyctime = np.cumsum(pxytimes)
 tiflctime = np.cumsum(tifltimes) 
 oortctime = np.cumsum(oorttimes)
-maxtime = max(max(pyctime), max(pxyctime),  max(tiflctime), max(oortctime))
+maxtime = max(max(rndctime), max(pyctime), max(pxyctime),  max(tiflctime), max(oortctime))
 
 x_ticks = np.arange(0, maxtime, 1500)
 y_ticks = np.arange(0, 1, 0.1)
-# plt.plot(rndctime, rndaccuracysmooth, '-.',  label = r'Random')
+plt.plot(rndctime, rndaccuracysmooth, '-.',  label = r'Random')
 plt.plot(pyctime, pyaccuracysmooth, '>',  label = r'P(y)')
 plt.plot(pxyctime, pxyaccuracysmooth, '-', label = r'P(X|y)')
 plt.plot(tiflctime, tiflaccuracysmooth, '--',  label = r'TiFL')
